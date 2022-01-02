95797de6f7d6d1d32449affb89ba2ff6c254bec6
Lalit Puri <80194925+laleet008@users.noreply.github.com>
2022-01-02 12:16:54 +0530

    Updates

---
+++ precache-manifest.c055e1b48b10a08f31ab57348f8c0a7d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae93acf19695a415fc8ccc9a259ef8b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cf32a82e35cc17c21d58",
+    "url": "/portfolio/static/css/2.002c45ea.chunk.css"
+  },
+  {
+    "revision": "674d33e31b6815d3bc53",
+    "url": "/portfolio/static/css/main.a1683149.chunk.css"
+  },
+  {
+    "revision": "cf32a82e35cc17c21d58",
+    "url": "/portfolio/static/js/2.9e35babd.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/portfolio/static/js/2.9e35babd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "674d33e31b6815d3bc53",
+    "url": "/portfolio/static/js/main.47050d30.chunk.js"
+  },
+  {
+    "revision": "7ed53fe796af37015373",
+    "url": "/portfolio/static/js/runtime-main.bd3ef9be.js"
+  },
+  {
+    "revision": "4bb8ca6c783e8ddb7e1b7fd71886d346",
+    "url": "/portfolio/static/media/explore.4bb8ca6c.jpg"
+  },
+  {
+    "revision": "197055a164a981fdd7a6a22b16d500ab",
+    "url": "/portfolio/static/media/greetings.197055a1.jpg"
+  },
+  {
+    "revision": "ce0ba1fda1962412a68fb6a7a302595f",
+    "url": "/portfolio/static/media/profile.ce0ba1fd.png"
+  },
+  {
+    "revision": "2153990cc2ea0d8f1f2572066c36bbd7",
+    "url": "/portfolio/static/media/work.2153990c.jpg"
+  }
+]);
